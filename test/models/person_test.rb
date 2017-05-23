5d00e555359b118ea0498eb8946f43d99b183949
Carlos Torrealba <calrrox@gmail.com>
2017-05-23 06:51:29 -0400

    hobbits

--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: people
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  age        :integer
+#  gender     :string
+#  alien      :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
